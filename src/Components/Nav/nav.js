a6243544bcc97da6b96ec81d69990aa3f02b44c9
Jesse <jessemckinney513@gmail.com>
2021-08-16 21:46:35 -0400

    yay IDK what Im doing???

--- src/Components/Nav/nav.js
+++ src/Components/Nav/nav.js
@@ -1,37 +1,41 @@
 import React from "react";
+import Wrapper from "../Wrapper/wrapper";
 import Styles from "./nav.module.css";
 
 function nav(props) {
   return (
     <>
-      <div className={Styles.wrapper}>
-        <div className="row">
-          <div className="col-sm-1" />
-          <div className="col-sm-1">
-            <h3 className={Styles.h3}>
-              <a className={Styles.href} href="/" alt="home">
-                <h4 className={Styles.h4}>Home</h4>
-              </a>
-            </h3>
+      <Wrapper>
+        <div className={Styles.wrapper}>
+          <div className="row">
+            <div className="col-sm-1" />
+            <div className="col-sm-1">
+              <h3 className={Styles.h3}>
+                <a className={Styles.href} href="/" alt="home">
+                  <h4 className={Styles.h4}>Home</h4>
+                </a>
+              </h3>
+            </div>
+            <div className="col-sm-5" />
+            <div id={Styles.portfolio} className="col-sm-2">
+              <h3 className={Styles.h3}>
+                <a className={Styles.href} href="/" alt="home">
+                  <h4 className={Styles.h4}>Portfolio</h4>
+                </a>
+              </h3>
+            </div>
+            <div id={Styles.chat} className="col-sm-2">
+              <h3 className={Styles.h3}>
+                <a className={Styles.href} href="/" alt="home">
+                  <h4 className={Styles.h4}>Let's Chat</h4>
+                  {/* At 713px-714px the NavBar Breaks and "Let's Chat" stacks on itself */}
+                </a>
+              </h3>
+            </div>
+            <div className="col-sm-1" />
           </div>
-          <div className="col-sm-7" />
-          <div className="col-sm-1">
-            <h3 className={Styles.h3}>
-              <a className={Styles.href} href="/" alt="home">
-                <h4 className={Styles.h4}>Portfolio</h4>
-              </a>
-            </h3>
-          </div>
-          <div className="col-sm-1">
-            <h3 className={Styles.h3}>
-              <a className={Styles.href} href="/" alt="home">
-                <h4 className={Styles.h4}>Let's Chat</h4>
-              </a>
-            </h3>
-          </div>
-          <div className="col-sm-1" />
         </div>
-      </div>
+      </Wrapper>
     </>
   );
 }
