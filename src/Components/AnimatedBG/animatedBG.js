5a961ba95e1f7f0e955c06b4aa35b5f2494c99db
Jesse <jessemckinney513@gmail.com>
2021-08-21 19:28:40 -0400

    yay

--- src/Components/AnimatedBG/animatedBG.js
+++ src/Components/AnimatedBG/animatedBG.js
@@ -37,12 +37,12 @@ class animatedBG extends Component {
       },
       easing: "linear",
       // easing: "easeInOutCirc",
-      duration: 4000,
-      // duration: 7000,
+      // duration: 4000,
+      duration: 7000,
       boxShadow: function () {
         return "10px 10px 50px rgba(0, 0, 0, 0.6)";
       },
-      delay: anime.stagger(10),
+      delay: anime.stagger(100),
       // Complete the animation effect on screen
       // complete: this.animateBlocks,
     });
