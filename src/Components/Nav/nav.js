bbb9499623f66b3f07b50658053ecf4ac876eeff
Jesse <jessemckinney513@gmail.com>
2021-08-23 19:43:03 -0400

    react-bootstrap was conflicting with reacts strictmode. had to rip it out and find another way for the collapsible navbar

--- src/Components/Nav/nav.js
+++ src/Components/Nav/nav.js
@@ -1,11 +1,5 @@
 import React, { Component } from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
-import Collapse from "react-bootstrap/Collapse";
 import Styles from "./nav.module.css";
-import NavStyle from "./MyNavBar.module.css";
 
 class nav extends Component {
   state = {
@@ -46,43 +40,6 @@ class nav extends Component {
   };
 
   render() {
-    const active = {
-      zIndex: "1001 !important",
-      backgroundColor: "#575f68ec",
-      color: "#fff",
-      position: "fixed",
-      height: "50px",
-      width: "100%",
-      visibility: "visible",
-      transition: "all 200ms ease-in",
-    };
-    const hidden = {
-      zIndex: "1001 !important",
-      backgroundColor: "#5e6f819a",
-      color: "#fff",
-      position: "fixed",
-      height: "50px",
-      width: "100%",
-      visibility: "hidden",
-      transition: "all 200ms ease-out",
-      transform: "translate(0, -100%)",
-    };
-    const bar = {
-      width: "35px",
-      height: "5px",
-      backgroundColor: "#1e7df0",
-      margin: "4px 0",
-      transition: "0.4s",
-    };
-    const changeBar1 = {
-      transform: "rotate(-45deg) translate(-7px, 6px)",
-    };
-    const changeBar2 = {
-      opacity: "0",
-    };
-    const changeBar3 = {
-      transform: "rotate(45deg) translate(-7px, -6px)",
-    };
     return (
       <>
         <div className={Styles.wrapper}>
@@ -124,97 +81,7 @@ class nav extends Component {
           </div>
           {/* Full Screen NavBar */}
           {/* Hamburger NavBar */}
-          <div className={Styles.hamburgerNavBar}>
-            <div
-              className={NavStyle.navBarWrapper}
-              style={this.state.show ? active : hidden}
-            >
-              <div className={NavStyle.navbar}>
-                <div className={NavStyle.buttonWrapper}>
-                  {/* NAVBAR CONTENT */}
-                  <Row className={NavStyle.navBarRow}>
-                    <Col>
-                      {/* NAVBAR BUTTON */}
-                      <Button
-                        className={NavStyle.menuButton}
-                        aria-controls="example-collapse-text"
-                        aria-expanded={this.state.open}
-                        onClick={this.toggleCollapse}
-                      >
-                        <div className={NavStyle.menuIconContainer}>
-                          <div
-                            className={NavStyle.bar1}
-                            style={this.state.changeNavBar ? changeBar1 : bar}
-                          ></div>
-                          <div
-                            className={NavStyle.bar2}
-                            style={this.state.changeNavBar ? changeBar2 : bar}
-                          ></div>
-                          <div
-                            className={NavStyle.bar3}
-                            style={this.state.changeNavBar ? changeBar3 : bar}
-                          ></div>
-                        </div>
-                      </Button>
-                      {/* NAVBAR BUTTON */}
-                    </Col>
-                  </Row>
-                  {/* NAVBAR CONTENT */}
-                  {/* COLLAPSE CONTENT */}
-                  <Collapse in={this.state.open}>
-                    <div
-                      id="example-collapse-text"
-                      className={NavStyle.collapsingContent}
-                    >
-                      <div className={NavStyle.contentDiv}>
-                        <Container className={NavStyle.contentContainer}>
-                          <Row className={NavStyle.contentHeader}>
-                            <h3 className={NavStyle.h3}>
-                              <a
-                                href="localhost:3000/solid"
-                                className={NavStyle.a}
-                                style={{ color: "#fff" }}
-                              >
-                                Jesse McKinney
-                              </a>
-                            </h3>
-                          </Row>
-                          <Row className={NavStyle.contentRow}>
-                            <Col className={NavStyle.contentCol}>
-                              <a
-                                href="localhost:3000/solid"
-                                className={NavStyle.a}
-                                style={{ color: "#fff" }}
-                              >
-                                About Me
-                              </a>
-                              <br />
-                              <a
-                                href="localhost:3000/solid"
-                                className={NavStyle.a}
-                                style={{ color: "#fff" }}
-                              >
-                                Portfolio
-                              </a>
-                              <br />
-                              <a
-                                href="localhost:3000/solid"
-                                className={NavStyle.a}
-                                style={{ color: "#fff" }}
-                              >
-                                Contact Me
-                              </a>
-                            </Col>
-                          </Row>
-                        </Container>
-                      </div>
-                    </div>
-                  </Collapse>
-                  {/* COLLAPSE CONTENT */}
-                </div>
-              </div>
-            </div>
-          </div>
+          <div className={Styles.hamburgerNavBar}></div>
           {/* Hamburger NavBar */}
         </div>
       </>
