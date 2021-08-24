ed25ee6388d45ed81b060ea28693328dfee1783c
Jesse <jessemckinney513@gmail.com>
2021-08-23 23:06:23 -0400

    trying to mske my own hamburger bar now without the screw ups of react-bootstrap crap

--- src/Components/Nav/nav.js
+++ src/Components/Nav/nav.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Button from "../Button/button";
 import Styles from "./nav.module.css";
 
 class nav extends Component {
@@ -19,12 +20,14 @@ class nav extends Component {
   }
 
   componentDidUpdate() {
+    // console.log("show: " + this.state.show);
     // console.log("open: " + this.state.open);
-    console.log("show: " + this.state.show);
     // console.log("chang: " + this.state.changeNavBar);
-    if (this.state.scrollPos > this.state.prevScrollPos) {
-      // console.log("Hello");
-    }
+    //
+    // if (this.state.scrollPos > this.state.prevScrollPos) {
+    // console.log("Hello");
+    // }
+    //
   }
 
   onscroll = () => {
@@ -38,22 +41,65 @@ class nav extends Component {
       this.setState({
         prevScrollPos: this.state.scrollPos,
       });
+      // console.log("show: " + this.state.show);
     }
   };
 
   toggleCollapse = (e) => {
     e.preventDefault();
-    this.setState({ changeNavBar: !this.state.changeNavBar });
-    if (this.state.open === false) {
-      this.setState({ open: true });
-    } else if (this.state.open === true) {
-      this.setState({ open: false });
-    } else {
-      this.setState({ open: false });
-    }
+    console.log("Clicked On");
   };
 
+  // toggleCollapse = (e) => {
+  //   e.preventDefault();
+  //   this.setState({ changeNavBar: !this.state.changeNavBar });
+  //   if (this.state.open === false) {
+  //     this.setState({ open: true });
+  //   } else if (this.state.open === true) {
+  //     this.setState({ open: false });
+  //   } else {
+  //     this.setState({ open: false });
+  //   }
+  // };
+
   render() {
+    const active = {
+      zIndex: "1001 !important",
+      backgroundColor: "#575f68ec",
+      color: "#fff",
+      position: "fixed",
+      height: "50px",
+      width: "100%",
+      visibility: "visible",
+      transition: "all 200ms ease-in",
+    };
+    const hidden = {
+      zIndex: "1001 !important",
+      backgroundColor: "#5e6f819a",
+      color: "#fff",
+      position: "fixed",
+      height: "50px",
+      width: "100%",
+      visibility: "hidden",
+      transition: "all 200ms ease-out",
+      transform: "translate(0, -100%)",
+    };
+    const bar = {
+      width: "35px",
+      height: "5px",
+      backgroundColor: "#fff",
+      margin: "4px 0",
+      transition: "0.4s",
+    };
+    const changeBar1 = {
+      transform: "rotate(-45deg) translate(-7.5px, 6px)",
+    };
+    const changeBar2 = {
+      opacity: "0",
+    };
+    const changeBar3 = {
+      transform: "rotate(45deg) translate(-7px, -6px)",
+    };
     return (
       <>
         <div className={Styles.wrapper}>
@@ -95,10 +141,28 @@ class nav extends Component {
           </div>
           {/* Full Screen NavBar */}
           {/* Hamburger NavBar */}
-          <div className={Styles.hamburgerNavBar}>
+          <div className={Styles.hamburgerNavBarWrapper}>
             <div id={Styles.brgrRow} className="row">
               <div id={Styles.brgrCol} className="col-md-12">
-                Hello World
+                <Button
+                  className={Styles.brgrButton}
+                  onClick={this.toggleCollapse}
+                >
+                  <div className={Styles.brgrIcon}>
+                    <div
+                      className={Styles.bar1}
+                      style={this.state.changeNavBar ? changeBar1 : bar}
+                    ></div>
+                    <div
+                      className={Styles.bar2}
+                      style={this.state.changeNavBar ? changeBar2 : bar}
+                    ></div>
+                    <div
+                      className={Styles.bar3}
+                      style={this.state.changeNavBar ? changeBar3 : bar}
+                    ></div>
+                  </div>
+                </Button>
               </div>
             </div>
           </div>
